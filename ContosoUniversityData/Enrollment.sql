adcb7f83e651ed069d9dd9208efe6c6e7c7ffb0e
Jacob Russell <jacob.russell82@outlook.com>
2018-10-25 22:49:36 -0400

    Add project files.

---
+++ ContosoUniversityData/Enrollment.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Enrollment]
+(
+	[EnrollementID] INT IDENTITY (1,1) NOT NULL,
+	[Grade]			Decimal (3, 2) NULL,
+	[CourseID]		INT NOT NULL,
+	[StudentID]		INT NOT NULL
+	PRIMARY KEY CLUSTERED ([EnrollementID] ASC),
+	CONSTRAINT [FK_dbo.Enrollment_dbo.Course_CourseID] FOREIGN KEY ([CourseID])
+		REFERENCES [dbo].[Course] ([CourseID]) ON DELETE CASCADE,
+	CONSTRAINT [FK_dbo.Enrollment_dbo.Student_StudentID] FOREIGN KEY ([StudentID])
+		REFERENCES [dbo].[Student] ([StudentID]) ON DELETE CASCADE
+)
